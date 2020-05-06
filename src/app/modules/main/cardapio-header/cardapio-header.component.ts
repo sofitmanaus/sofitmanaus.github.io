f5bdeb3d39733f9f40b5849aaf5c06719608da1f
Kristhian Aguilar <kris.andre7@gmail.com>
2020-05-06 14:52:10 -0400

    Jumbotron implementado

---
+++ src/app/modules/main/cardapio-header/cardapio-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardapio-header',
+  templateUrl: './cardapio-header.component.html',
+  styleUrls: ['./cardapio-header.component.scss']
+})
+export class CardapioHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/main/cardapio/cardapio.component.ts
+++ src/app/modules/main/cardapio/cardapio.component.ts
@@ -3,6 +3,7 @@ import { FoodModel } from 'src/app/core/models/food.model';
 import { Cloudinary } from '@cloudinary/angular-5.x';
 import { StateChange } from 'ng-lazyload-image';
 import { environment } from 'src/environments/environment';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-cardapio',
@@ -16,9 +17,10 @@ export class CardapioComponent implements OnInit {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
       options: [],
       price: 22,
-      title: 'Tortilha Integrais',
+      title: 'Tortilhas Integrais',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Ftortillas.png?alt=media`,
-      loading: true
+      loading: true,
+      express: true
     },
     {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
@@ -35,7 +37,8 @@ export class CardapioComponent implements OnInit {
       price: 26,
       title: 'Granola Funcional',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Fgranola.jpg?alt=media`,
-      loading: true
+      loading: true,
+      express: true
     },
     {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
@@ -56,7 +59,8 @@ export class CardapioComponent implements OnInit {
       price: 22,
       title: 'Iogurte Caseiro',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Fiogurte.jpg?alt=media`,
-      loading: true
+      loading: true,
+      express: true
     },
     {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
@@ -77,7 +81,8 @@ export class CardapioComponent implements OnInit {
       price: 25,
       title: 'Leites Vegetais',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Fleite_vegetal.jpg?alt=media`,
-      loading: true
+      loading: true,
+      express: false
     },
     {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
@@ -98,7 +103,8 @@ export class CardapioComponent implements OnInit {
       price: 62,
       title: 'Quiches',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Fquiche.jpg?alt=media`,
-      loading: true
+      loading: true,
+      express: false
     },
     {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
@@ -111,7 +117,8 @@ export class CardapioComponent implements OnInit {
       price: 80,
       title: 'Mini Quiches',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Fmini_quiche.jpg?alt=media`,
-      loading: true
+      loading: true,
+      express: false
     },
     {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
@@ -136,7 +143,8 @@ export class CardapioComponent implements OnInit {
       price: 72,
       title: 'Cupcakes',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Fcupcake.jpg?alt=media`,
-      loading: true
+      loading: true,
+      express: false
     },
     {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
@@ -144,7 +152,8 @@ export class CardapioComponent implements OnInit {
       price: 65,
       title: 'Cheesecake Low-carb',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Fcheesecake.jpg?alt=media`,
-      loading: true
+      loading: true,
+      express: false
     },
     {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
@@ -152,7 +161,8 @@ export class CardapioComponent implements OnInit {
       price: 65,
       title: 'Torta de Nozes',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Ftorta_nozes.jpg?alt=media`,
-      loading: true
+      loading: true,
+      express: false
     },
     {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
@@ -173,7 +183,8 @@ export class CardapioComponent implements OnInit {
       price: 12,
       title: 'Frutas Desidratadas',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Ffruta_desidratada.jpg?alt=media`,
-      loading: true
+      loading: true,
+      express: false
     },
     {
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec velit et nisi pellentesque pharetra. Cras lacus neque, porttitor sit amet ex in, hendrerit tincidunt orci.',
@@ -181,15 +192,23 @@ export class CardapioComponent implements OnInit {
       price: 15,
       title: 'Mix de Nuts Caramelizadas',
       image: `https://firebasestorage.googleapis.com/v0/b/${environment.firebaseConfig.storageBucket}/o/food%2Fnuts_caramelizados.jpg?alt=media`,
-      loading: true
+      loading: true,
+      express: false
     }
   ]
 
-  constructor(private cloudinary: Cloudinary) { }
+  today= new Date();
+  todayTime: number;
+  isOpen = false
 
-  imageCallback(event: StateChange, index: number) {
-    if (event.reason === 'loading-succeeded') {
-      this.foods[index].loading = false;
+  constructor(private cloudinary: Cloudinary) {
+    this.todayTime = parseInt(formatDate(this.today, 'H:mm', 'en-US'));
+    if (this.todayTime >= 8 && this.todayTime <= 18) {
+      console.log('ABERTO')
+      this.isOpen = true
+    } else {
+      this.isOpen = false
+      console.log('FECHADO')
     }
   }
 
